970b3b7090d6ee5e901e2b465079202da5ff8c79
Jose Teotonio <teo.nett@icloud.com>
2020-03-05 21:12:25 -0300

    Implementacao de consulta dos dados do usuario

--- js/main.js
+++ js/main.js
@@ -1,10 +1,65 @@
 
 function formSubmit(){
     $('form').submit(event => {
+
         event.preventDefault();
+
         const username = $('input').val().toLowerCase();
+        
+        loadUsers(username);
         userRepos(username);
+
       })
+}
+
+$(formSubmit());
+
+function loadUsers(username){
+
+    var xhr = new XMLHttpRequest();
+        
+    xhr.open('GET' , `https://api.github.com/users/${username}` , true);
+
+    xhr.onload = function(){
+        if(this.status == 200){
+
+            var users = JSON.parse(this.responseText);
+
+            var output = `
+            <h3 class="head-user">${users.name}</h3>
+            <table>
+                <tr>
+                    <td rowspan="5"><img src="${users.avatar_url}"></td>
+                    <td></td>
+                    <td>Repositórios : ${users.public_repos}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Gists : ${users.public_gists}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Location : ${users.location}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Bio : ${users.bio}</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a class="btn btn-default" target="_blank" href="${users.html_url}">Visit Github</a></td>
+                </tr>             
+            </table>
+            
+            <th><h3 class="head-user">Avalilable Repositories List</h3><th>
+
+            `
+
+            document.getElementById('users').innerHTML = output;
+        }
+
+    }
+        xhr.send();
 }
 
 function userRepos(username){
@@ -16,7 +71,6 @@ function userRepos(username){
           }
           throw new Error(response.statusText);
       })
-      .then(responseJson => displayOwner(responseJson) )
       .then(responseJson => displayRepos(responseJson))
       .catch(err => {alert(`There was an error! Something went wrong: ${err.message}`)});
 }
@@ -41,36 +95,30 @@ function displayRepos(responseJson){
     
 };
 
-function displayOwner(responseJson) {
-    
-    responseJson.displayOwner( owner =>{
-        let {
-            login
-        } = owner;
-
-        $('.owner').append(createTemplate(login));
-    });
-}
-
-const createOwnerTemplate = function(login){
-    let template = `
-    <p>${login}</p>
-    `
-}
-
 const createTemplate = function(repo_name, url, dtcreated, dtupdated, description) {
+
     let template = `
     <section>
-      <p class="p0"><a href="${url}" title="Click here to redirect to the project">${repo_name.toUpperCase().split('-').join(' ')}</a></p>
-      <p class="desc"> ${description} </p>
-      <ul>
-        <li>Date Created: ${dtcreated.getMonth("00")}/${dtcreated.getDate("00")}/${dtcreated.getFullYear()}</li>
-        <li>Last update: ${dtupdated.getMonth()}/${dtupdated.getDate()}/${dtupdated.getFullYear()}</li>
-      </ul>
+        <table>
+            <tr>
+                <td colspan="4" class="p0"><a href="${url}" title="Click here to redirect to the project">${repo_name.toUpperCase().split('-').join(' ')}</a></td>
+            </tr>
+            <tr><td colspan="4" class="desc">${description}</td></tr>
+            <tr>
+                <td>Date Created:</td>
+                <td style="text-align: left;">${dtcreated.getMonth()}/${dtcreated.getDate()}/${dtcreated.getFullYear()}</td>
+                <td>Last update:</td>
+                <td style="text-align: left;">${dtupdated.getMonth()}/${dtupdated.getDate()}/${dtupdated.getFullYear()}</td>
+            </tr>
+        </table>
     </section>
     `;
+
+    
     return template;
 };
 
-$(formSubmit());
+
+
+
 
